dd27005885e403af3a5460d619e0a444bd767e6f
widever <rkusnir856@gmail.com>
2026-04-30 20:42:20 +0300

    Update marking and comb grading

--- optimizer/mp/optimizer/benchmark.py
+++ optimizer/mp/optimizer/benchmark.py
@@ -11,20 +11,12 @@ data_dir = os.path.join(data_dir, f"optimize_main_dir")
 
 def super_benchmark(combs: list[CombGrade] = None):
     # combs: list[CombGrade] = [
-    #     CombGrade(
-    #         comb=('#tag_btc_doge_log_return_ratio_lt_-1.316', '#tag_ada_trend_kind_eq_falling',
-    #               '#tag_sui_trend_ch_from_peak_lt_-0.011', '#tag_avg_log_return_ratio_lt_0.468',
-    #               '#tag_btc_ada_log_return_ratio_lt_0.451', '#tag_avg_ch_from_peak_ratio_gt_-0.098',
-    #               '#tag_eth_doge_log_return_ratio_lt_1.081', '#tag_doge_sui_ch_from_peak_ratio_lt_0.999',
-    #               '#tag_eth_doge_ch_from_peak_ratio_gt_0.59', '#tag_eth_ada_ch_from_peak_ratio_lt_0.904'),
-    #         score=0.35294117647058826, props={'loss': 60, 'profit': 10, 'total': 70}, verify_grade=None),
-    #     CombGrade(comb=('#tag_ada_trend_ch_from_peak_gt_0.048', '#tag_avg_log_return_ratio_lt_0.468',
-    #                     '#tag_xrp_trend_ch_from_peak_gt_0.052', '#tag_btc_doge_ch_from_peak_ratio_lt_0.828',
-    #                     '#tag_sui_trend_ch_from_peak_gt_-0.042', '#tag_btc_ada_ch_from_peak_ratio_gt_-3.617',
-    #                     '#tag_btc_xrp_ch_from_peak_ratio_gt_-3.48', '#tag_eth_ada_ch_from_peak_ratio_gt_-1.563',
-    #                     '#tag_eth_xrp_ch_from_peak_ratio_gt_-2.18', '#tag_doge_ada_ch_from_peak_ratio_gt_-2.031'),
-    #               score=0.8973063973063973, props={'loss': 11, 'profit': 483, 'total': 494}, verify_grade=None)
-    #
+    #     CombGrade(comb=('#tag_ada_ampl_gt_0.012', '#tag_avg_drop_from_high_gt_0.03',
+    #                     '#tag_btc_doge_log_return_ratio_lt_0.513', '#tag_eth_ampl_gt_0.008',
+    #                     '#tag_btc_trend_kind_eq_falling', '#tag_doge_xrp_drop_from_high_ratio_lt_1.555',
+    #                     '#tag_btc_xrp_ampl_ratio_lt_0.672', '#tag_eth_ada_ampl_ratio_lt_1.227',
+    #                     '#tag_btc_ada_drop_from_high_ratio_lt_1.57', '#tag_btc_rise_from_low_lt_0.011'),
+    #               score=0.411522633744856, props={'loss': 93, 'profit': 50, 'total': 143}, verify_grade=None)
     # ]
 
     marked_points_df = pd.read_csv(f"{data_dir}/marked_points_frozen.csv")
--- optimizer/mp/optimizer/for_point.py
+++ optimizer/mp/optimizer/for_point.py
@@ -16,12 +16,6 @@ def build_marked_points_df_for_point(symbol: str):
         timestamp = int(row["timestamp"])
         point_values_ = mark.point_values(symbol, timestamp)
 
-        close_price = float(row["close"])
-        low_price = float(row["low"])
-        high_price = float(row["high"])
-        timestamp = int(row["timestamp"])
-        ampl = (high_price - low_price) / low_price
-
         new_opened_marked_point = mark.MarkedPoint(
             index=idx,
             timestamp=timestamp,
@@ -29,14 +23,11 @@ def build_marked_points_df_for_point(symbol: str):
             sl_price_limit=0,
             sell_price_limit=0,
 
-            rising=row["rising"],
-            falling=row["falling"],
-            flat=row["flat"],
             peak_up=row["peak_up"],
             peak_down=row["peak_down"],
             change_from_last_peak=row["change_from_last_peak"],
             len_from_last_peak=row["len_from_last_peak"],
-            ampl=ampl
+            last_peak_type=row["last_peak_type"],
         )
         opened_points.append(new_opened_marked_point)
 
@@ -54,20 +45,12 @@ def build_marked_points_df_for_point(symbol: str):
 
 def check_combs_for_point() -> bool:
     combs: list[CombGrade] = [
-        CombGrade(
-        comb=('#tag_btc_doge_log_return_ratio_lt_-1.316', '#tag_ada_trend_kind_eq_falling',
-              '#tag_sui_trend_ch_from_peak_lt_-0.011', '#tag_avg_log_return_ratio_lt_0.468',
-              '#tag_btc_ada_log_return_ratio_lt_0.451', '#tag_avg_ch_from_peak_ratio_gt_-0.098',
-              '#tag_eth_doge_log_return_ratio_lt_1.081', '#tag_doge_sui_ch_from_peak_ratio_lt_0.999',
-              '#tag_eth_doge_ch_from_peak_ratio_gt_0.59', '#tag_eth_ada_ch_from_peak_ratio_lt_0.904'),
-        score=0.35294117647058826, props={'loss': 60, 'profit': 10, 'total': 70}, verify_grade=None),
-        CombGrade(comb=('#tag_ada_trend_ch_from_peak_gt_0.048', '#tag_avg_log_return_ratio_lt_0.468',
-                        '#tag_xrp_trend_ch_from_peak_gt_0.052', '#tag_btc_doge_ch_from_peak_ratio_lt_0.828',
-                        '#tag_sui_trend_ch_from_peak_gt_-0.042', '#tag_btc_ada_ch_from_peak_ratio_gt_-3.617',
-                        '#tag_btc_xrp_ch_from_peak_ratio_gt_-3.48', '#tag_eth_ada_ch_from_peak_ratio_gt_-1.563',
-                        '#tag_eth_xrp_ch_from_peak_ratio_gt_-2.18', '#tag_doge_ada_ch_from_peak_ratio_gt_-2.031'),
-                  score=0.8973063973063973, props={'loss': 11, 'profit': 483, 'total': 494}, verify_grade=None)
-
+        CombGrade(comb=('#tag_ada_ampl_gt_0.012', '#tag_avg_drop_from_high_gt_0.03',
+                        '#tag_btc_doge_log_return_ratio_lt_0.513', '#tag_eth_ampl_gt_0.008',
+                        '#tag_btc_trend_kind_eq_falling', '#tag_doge_xrp_drop_from_high_ratio_lt_1.555',
+                        '#tag_btc_xrp_ampl_ratio_lt_0.672', '#tag_eth_ada_ampl_ratio_lt_1.227',
+                        '#tag_btc_ada_drop_from_high_ratio_lt_1.57', '#tag_btc_rise_from_low_lt_0.011'),
+                  score=0.411522633744856, props={'loss': 93, 'profit': 50, 'total': 143}, verify_grade=None)
     ]
 
 
