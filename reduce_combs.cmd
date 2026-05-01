21a9997bde074bed65ac000eefd18d727afdbd91
widever <rkusnir856@gmail.com>
2026-05-01 18:01:41 +0300

    Add GPU accelerator for reducing combs count

---
+++ reduce_combs.cmd
@@ -0,0 +1,6 @@
+
+call 312venv\Scripts\activate
+
+set PYTHONPATH=%PYTHONPATH%;D:\AAAProjects\MarketPrediction2\market;D:\AAAProjects\MarketPrediction2\optimizer;D:\AAAProjects\MarketPrediction2\trade
+
+python -m mp.optimizer.comb_v2
